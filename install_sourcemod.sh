40d98dabcfb034ab9bf5db4ecd11b89f9a28ceec
Thomas Strametz <thomas.strametz2@fh-joanneum.at>
2021-03-28 17:48:40 +0200

    Minor bugfix

--- install_sourcemod.sh
+++ install_sourcemod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd ./tf2
+cd ./tf2/tf
 wget https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1144-linux.tar.gz
 wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6502-linux.tar.gz
 tar xzf mmsource-1.11.0-git1144-linux.tar.gz
